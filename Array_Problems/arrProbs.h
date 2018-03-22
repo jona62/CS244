a74eecbf66a696d5259d49b328386692cdfb31eb
jona62 <jonathanjamesm66@gmail.com>
2018-03-22 19:27:00 -0400

    Array Update

--- Array_Problems/arrProbs.h
+++ Array_Problems/arrProbs.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <iomanip>
 #include <ctime>
 using namespace std;
 #ifndef ARRPROBS_H
@@ -18,26 +19,29 @@ void randomizer(int arr[], int size){ //Used this to generate values for to put 
 
 
 void findGPA(int arr[], int size){ //Prints out a Letter grade based on a score
-  int grade=0; // Initiatization of the grade variable
+  int average_grade=0; // Initiatization of the grade variable
   for(int i=0; i<size; i++){
-    grade = arr[i]; // Holds the value of grade at every iteration
-    switch(grade){
-      case 0:
-        cout<<grade<<" - F\n"; //Prints an F if grade is 0
-        break;
-      case 1:
-        cout<<grade<<" - D\n"; //Prints a D if grade is 1
-        break;
-      case 2:
-        cout<<grade<<" - C\n"; //Prints a C if grade is 2
-        break;
-      case 3:
-        cout<<grade<<" - B\n"; //Prints a B if grade is 3
-        break;
-      case 4:
-        cout<<grade<<" - A\n"; //Prints an A if grade is 4
-        break;
-    }
+
+    average_grade += arr[i]; // Holds the value of grade at every iteration
+  }
+  average_grade = average_grade/size;
+  switch(average_grade){
+    case 0:
+      cout<<fixed<<setprecision(2);
+      cout<<"Your grade is "<<average_grade<<".0 & Your GPA is F\n"; //Prints an F if grade is 0
+      break;
+    case 1:
+      cout<<"Your grade is "<<average_grade<<".0 & Your GPA is D\n"; //Prints a D if grade is 1
+      break;
+    case 2:
+      cout<<"Your grade is "<<average_grade<<".0 & Your GPA is C\n"; //Prints a C if grade is 2
+      break;
+    case 3:
+      cout<<"Your grade is "<<average_grade<<".0 & Your GPA is B\n"; //Prints a B if grade is 3
+      break;
+    case 4:
+      cout<<"Your grade is "<<average_grade<<".0 & Your GPA is A\n"; //Prints an A if grade is 4
+      break;
   }
 }
 
